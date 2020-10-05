33bfd326b7334c250117b4bc88b8493ad0c4c12e
yrrah1212 <yrrah1212@gmail.com>
2020-10-05 15:47:47 -0600

    code works, arduino code not up to date

--- assignment2.py
+++ assignment2.py
@@ -6,6 +6,17 @@ import board
 import busio
 import adafruit_character_lcd.character_lcd_rgb_i2c as character_lcd
 from computer_vision import aruco_detection as ar
+import sys
+
+
+def display_tag(t):
+    lcd.cursor_position(11, 0)
+    lcd.message = f"{t}     "
+
+
+def display_position(pos):
+    lcd.cursor_position(10, 1)
+    lcd.message = f"{pos}     "
 
 
 # writes one byte to the Arduino
@@ -16,8 +27,11 @@ def write_number(value):
 
 # reads one bytes from the Arduino
 def read_number():
-    number = bus.read_byte(arduino_address)
-    return number
+    number = bus.read_i2c_block_data(arduino_address, 0, 5)
+    value = chr(number[0]) + chr(number[1]) + chr(number[2]) + chr(number[3]) + chr(number[4])
+
+
+    return value
 
 
 bus = smbus2.SMBus(1)
@@ -33,30 +47,40 @@ lcd.color = [0, 100, 0]  # Set LCD color to green
 arduino_address = 0x04
 
 
-def display_tag(tag):
-    lcd.clear()
-    lcd.message = f"set point: {tag}"
-
 if __name__ == '__main__':
-    image = ar.capture_image()
-    _, ids = ar.detect_markers(image, verbose=False)
-    print(f'The ids are {ids}')
-
-    if ids is not None:
-        tag = ids[0][0]
-    else:
-        tag = 47
-
-    display_tag(tag)
-    write_number(tag)
-    time.sleep(1)
-    
-
+    old_tag = 1
+    valid_tags = [1, 2, 3, 4]
+    lcd.message = f"set point:"
     lcd.message = f"\nPosition:"
-    time.sleep(1)
-    
-    for i in range(20):
-        returned = read_number()
-        lcd.cursor_position(10,1)
-        lcd.message = f"{i}     "
-        time.sleep(.25)
+
+    while True:
+        try:
+            image = ar.capture_image()
+            ids = None
+            _, ids = ar.detect_markers(image, verbose=False)
+
+            if ids is not None:
+                tag = ids[0][0]
+                print(f'The tag is {tag}')
+                if tag in valid_tags:
+                    display_tag(tag)
+                    write_number(tag)
+                    old_tag = tag
+
+            returned = read_number()
+            display_position(returned)
+            # time.sleep(.1)
+        except KeyboardInterrupt:
+            sys.exit(0)
+
+        except OSError as err:
+            if err.errno == 121:
+                # input("Press enter to reconnect to the I2C Bus")
+                print("Reconnecting to the I2C bus")
+                time.sleep(.5)
+                bus = smbus2.SMBus(1)
+
+
+
+
+
